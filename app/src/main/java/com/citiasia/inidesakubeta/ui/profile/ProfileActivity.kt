bab7116b0a04022454c5a8c3512d66811b7c72a9
Abdul Majid Refindo <abdulmajirefindo@gmail.com>
2023-11-28 14:12:16 +0700

    initialize profile data diri

---
+++ app/src/main/java/com/citiasia/inidesakubeta/ui/profile/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.citiasia.inidesakubeta.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.citiasia.inidesakubeta.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
