17c62637d960e3d79462a40751eaee9e2ddc3b41
luthfior <luthfiraama@gmail.com>
2023-11-28 16:56:33 +0700

    signup (daftar akun, otp, biodata, pilih wilayah)

---
+++ app/src/main/java/com/citiasia/inidesakubeta/ui/sign/signUp/CompleteProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.citiasia.inidesakubeta.ui.sign.signUp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.citiasia.inidesakubeta.R
+
+class CompleteProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complete_profile)
+    }
+}
