e1ae5eef64be3dcaaae406edbb8da58d8e4904d0
DenFadhli <denfdhl5702@gmail.com>
2023-12-07 16:28:00 +0700

    pesona desa

--- app/src/main/java/com/citiasia/inidesakubeta/BerandaFragment.kt
+++ app/src/main/java/com/citiasia/inidesakubeta/BerandaFragment.kt
@@ -1,16 +1,10 @@
 package com.citiasia.inidesakubeta
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.citiasia.inidesakubeta.databinding.FragmentBerandaBinding
-import com.citiasia.inidesakubeta.databinding.FragmentPpobBinding
-import com.citiasia.inidesakubeta.ui.ppob.bpjs.BpjsActivity
-import com.citiasia.inidesakubeta.ui.ppob.listrikAir.ListrikAirActivity
-import com.citiasia.inidesakubeta.ui.ppob.pulsa.PulsaActivity
 import com.denzcoskun.imageslider.ImageSlider
 import com.denzcoskun.imageslider.models.SlideModel
 
@@ -25,21 +19,24 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class BerandaFragment : Fragment() {
-
-    private var _binding: FragmentBerandaBinding? = null
-    private val binding get() = _binding !!
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentBerandaBinding.inflate(layoutInflater, container, false)
-        return binding.root
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_beranda, container, false)
     }
 
     val imageList = ArrayList<SlideModel>()
@@ -53,23 +50,28 @@ class BerandaFragment : Fragment() {
         imageList.add(SlideModel(R.drawable.carousel_4))
         imageList.add(SlideModel(R.drawable.carousel_5))
 
-        val imageSlider = view.findViewById<ImageSlider>(R.id.imageSlider)
+        val imageSlider = view.findViewById<ImageSlider>(R.id.imageSlider2)
         imageSlider.setImageList(imageList)
 
-        iconClick()
     }
 
-    private fun iconClick() {
-        with(binding) {
-            btnPesonaDesa.setOnClickListener {
-                toPesonaDesa()
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BerandaFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            BerandaFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
             }
-        }
     }
-
-    private fun toPesonaDesa() {
-        val intent = Intent(requireActivity(), PesonaDesaActivity::class.java)
-        startActivity(intent)
-    }
-
 }
